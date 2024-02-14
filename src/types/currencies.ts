ed83f66e1daeadd843cd21058d5c91bd7d013eac
Yuta Kawamura <ytkwmr18@icloud.com>
2024-02-14 10:28:51 -0800

    Create cosntatns and types

---
+++ src/types/currencies.ts
@@ -0,0 +1,36 @@
+export type CurrencyExchange = {
+    AUD: number;
+    BGN: number;
+    BRL: number;
+    CAD: number;
+    CHF: number;
+    CNY: number;
+    CZK: number;
+    DKK: number;
+    EUR: number;
+    GBP: number;
+    HKD: number;
+    HRK: number;
+    HUF: number;
+    IDR: number;
+    ILS: number;
+    INR: number;
+    ISK: number;
+    JPY: number;
+    KRW: number;
+    MXN: number;
+    MYR: number;
+    NOK: number;
+    NZD: number;
+    PHP: number;
+    PLN: number;
+    RON: number;
+    RUB: number;
+    SEK: number;
+    SGD: number;
+    THB: number;
+    TRY: number;
+    USD: number;
+    ZAR: number;
+  };
+  
