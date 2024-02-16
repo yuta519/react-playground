2320b1cc805b4179d9c2dead8df057d59f375b50
Yuta Kawamura <ytkwmr18@icloud.com>
2024-02-16 11:44:51 -0800

    Create call api page

--- src/pages/ApiCall.tsx
+++ src/pages/ApiCall.tsx
@@ -1,14 +1,29 @@
-import axios from "axios";
 import { useEffect, useState } from "react";
+import { useDummyData } from "../hooks/useDummyData";
 
 export default function ApiCall () {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState({ id: 0, quote: '', author: ''});
+  const [id, setId] = useState(1);
+  const { fetchQuoteById } = useDummyData()
 
   useEffect(() => {
-    axios.get("/api/data").then((response: any) => {setData(response)})
-  }, []);
+    data.id !== id && (async() => setData(await fetchQuoteById(id)))();
+  }, [id]);
   
+  const handleClick = () => setId(Math.ceil(Math.random() * 100));
 
-  return (<>{data.length ? (<>aaa</>) : (<>Nothing</>)}</>)
+  return (
+    <>
+      {!!data.id ? (
+        <>
+          <h3>{data.author}</h3>
+          <div>{data.quote}</div>
+          <button onClick={handleClick}>Change Article</button>
+        </>
+      ) : (
+        <>Loading...</>
+      )}
+    </>
+  )
   ;
 }
